dd4d625fccefe8918df983f8b9d610588830de93
Nick <nicknyler@gmail.com>
2025-06-14 20:25:10 +0300

    added server/app.py

---
+++ server/app.py
@@ -0,0 +1,22 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from .config import Config
+from .models import db
+from .controllers.restaurant_controller import restaurant_bp
+from .controllers.pizza_controller import pizza_bp
+from .controllers.restaurant_pizza_controller import restaurant_pizza_bp
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config)
+    db.init_app(app)
+    Migrate(app, db)
+
+    app.register_blueprint(restaurant_bp)
+    app.register_blueprint(pizza_bp)
+    app.register_blueprint(restaurant_pizza_bp)
+
+    return app
+
+app = create_app()
